63ad18b0c463c1199af422789a313a82c9aab687
Richard O'Dwyer <richard@richard.do>
2013-01-03 00:45:28 +0000

    Initial commit

---
+++ tests/trailer_test.py
@@ -0,0 +1,18 @@
+from imdbpie import Imdb
+import re
+
+imdb = Imdb({'anonymize' : False})
+movie = imdb.find_movie_by_id("tt1210166")
+
+def run_tests():
+    global imdb
+
+    print('have a trailer_url:')
+    match = re.findall(r'http://ia.media-imdb.com/images/.*/', movie.trailer_url)[0]
+    if match:
+        print('passed')
+
+
+
+if __name__ == '__main__':
+    run_tests()
