02a0c2b925118dea596ecc7461a22f30aaa749f3
richard@richard.do <fofput2>
2013-01-25 20:48:48 +0000

    Fixed bug in find_by_title, caused some search results to be lost, now corrected.

--- imdbpie.py
+++ imdbpie.py
@@ -78,7 +78,6 @@ class Imdb:
         r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_1 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8B5097d Safari/6531.22.7'})
         return json.loads(r.text)
 
-
 class Person:
     def __init__( self, options=None):
         if options is None:
@@ -129,4 +128,3 @@ class Movie:
         if "trailer" in options and options["trailer"]["encodings"]:
             for k, v in options["trailer"]["encodings"].items():
                 self.trailers[v["format"]] = v["url"]
-
