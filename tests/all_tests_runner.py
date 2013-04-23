9303c8564819cd9f0a3d3fb8db096cde008415dc
richard <richard@richard.do>
2013-04-23 20:14:57 +0100

    Added test runner

---
+++ tests/all_tests_runner.py
@@ -0,0 +1,15 @@
+import unittest
+
+def load_tests(loader, tests, pattern):
+    """
+    Find and load all tests in dir
+    """
+    suite = unittest.TestSuite()
+    for all_tests in unittest.defaultTestLoader.discover('./', pattern='*_test.py'):
+        for test in all_tests:
+            suite.addTests(test)
+    return suite
+
+
+if __name__ == '__main__':
+    unittest.main()
