28f77098fec45132e847c54c982c6b01b0b5c865
richard <richard@richard.do>
2013-04-30 23:22:00 +0100

    Raise error if validate_id() fails

--- imdbpie/imdbpie.py
+++ imdbpie/imdbpie.py
@@ -101,7 +101,7 @@ class Imdb:
                 id_num = id_num.zfill(7)
             return 'tt' + id_num
         else:
-            return False
+            return ValueError('Could not validate ID')
 
     def find_by_title(self, title):
         default_find_by_title_params = {'json': '1',
