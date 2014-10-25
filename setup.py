ee2a9baf5306a7851af23742c377eec5b0f1952e
Richard O'Dwyer <richard@richard.do>
2014-10-26 00:34:32 +0100

    1.5.4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='imdbpie',
-    version='1.5.3',
+    version='1.5.4',
     packages=find_packages('src', exclude=('tests',)),
     package_dir={'': 'src'},
     include_package_data=True,
