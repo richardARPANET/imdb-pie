f3b97103669edfc838175cb5f559273af711fbf3
Richard O'Dwyer <richard@richard.do>
2015-03-01 22:39:40 +0000

    Updates user agents list, fixing invalid agent string

--- src/imdbpie/imdbpie.py
+++ src/imdbpie/imdbpie.py
@@ -8,6 +8,9 @@ import os
 import random
 import re
 import time
+
+import requests
+
 # handle python 2 and python 3 imports
 try:
     from urllib.parse import urlencode
@@ -15,7 +18,6 @@ try:
 except ImportError:
     from urllib import urlencode
     import HTMLParser as htmlparser
-import requests
 
 logger = logging.getLogger(__name__)
 
@@ -28,7 +30,7 @@ USER_AGENTS = (
     'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0_1 like Mac OS X) '
     'AppleWebKit/534.46 (KHTML, like Gecko) Mobile/9A406',
     'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0_1 like Mac OS X) '
-    'AppleWebKit/534.46 (KHTML, like Gecko) Ver sion/5.1 Mobile/9A405 '
+    'AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A405 '
     'Safari/7534.48.3',
     'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0_1 like Mac OS X) '
     'AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A405 '
@@ -47,6 +49,11 @@ USER_AGENTS = (
     'Mozilla/5.0(iPhone; U; CPU iPhone OS 4_1 like Mac OS X; en-us)'
     'AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8B5097d '
     'Safari/6531.22.7',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0_2 like Mac OS X) '
+    'AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A366 '
+    'Safari/600.1.4'
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.4'
+    ' (KHTML, like Gecko) Version/8.0 Mobile/12A366 Safari/600.1.4',
 )
 
 
