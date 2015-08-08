c839f192b0732125a47f2ca36d5c551536ba2fdd
Richard O'Dwyer <richard@richard.do>
2015-08-08 14:29:57 +0100

    Preparing release 4.0.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='imdbpie',
-    version='4.0.0.dev0',
+    version='4.0.0',
     packages=find_packages('src', exclude=('tests',)),
     package_dir={'': 'src'},
     include_package_data=True,
