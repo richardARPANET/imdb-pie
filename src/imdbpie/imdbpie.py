e300c4353332a182b6172bb4bc770609a8b165ec
Labrys of Knossos <Labrys.git@gmail.com>
2018-03-15 23:31:16 -0400

    Fix incorrect super call
    Closes #71

--- src/imdbpie/imdbpie.py
+++ src/imdbpie/imdbpie.py
@@ -61,7 +61,7 @@ class Imdb(Auth):
 
     def __getattr__(self, method_name):
         if method_name not in _SIMPLE_GET_ENDPOINTS:
-            return super(Imdb).__getattr__(method_name)
+            return super(Imdb, self).__getattr__(method_name)
         return self._simple_get_method(
             method=method_name, path=_SIMPLE_GET_ENDPOINTS[method_name]
         )
