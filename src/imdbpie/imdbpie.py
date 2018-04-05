2f3bbf769a30937388be67d5c867b81ff5c90d8a
Richard O'Dwyer <richard@richard.do>
2018-04-05 22:28:36 +0100

    Fix package docs

--- src/imdbpie/imdbpie.py
+++ src/imdbpie/imdbpie.py
@@ -207,20 +207,17 @@ class Imdb(Auth):
 
     def get_title_top_crew(self, imdb_id):
         """
-        Request detailed information about title's top crew (ie: directors, writters, etc.).
+        Request detailed information about title's top crew
+        (ie: directors, writters, etc.).
 
         :param imdb_id: The imdb id including the TT prefix.
         """
         logger.info('called get_title_top_crew %s', imdb_id)
-
         self.validate_imdb_id(imdb_id)
-
-        params = {
-            'tconst': imdb_id,
-        }
-
+        params = {'tconst': imdb_id}
         return self._get(urljoin(
-            BASE_URI, '/template/imdb-android-writable/7.3.top-crew.jstl/render'
+            BASE_URI,
+            '/template/imdb-android-writable/7.3.top-crew.jstl/render'
         ), params=params)
 
     @staticmethod
@@ -291,7 +288,6 @@ class Imdb(Auth):
 
         if resp_dict.get('error'):
             return None
-
         return resp_dict
 
     def _redirection_title_check(self, imdb_id):
