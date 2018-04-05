a88b348d43ad52380e1e165b5d276f4c42fe7464
Richard O'Dwyer <richard@richard.do>
2018-04-05 23:17:37 +0100

    Correct license and use rst format since pypi has a but with markdown

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 with open('requirements.txt') as f:
     install_requires = f.read().splitlines()
 
-with open('README.md', 'r') as rm_file:
+with open('README.rst', 'r', encoding='utf-8') as rm_file:
     readme = rm_file.read()
 
-with open('HISTORY.md', 'r') as hist_file:
+with open('HISTORY.rst', 'r', encoding='utf-8') as hist_file:
     history = hist_file.read()
 
 setup(
@@ -32,9 +32,8 @@ setup(
     ),
     author='Richard O\'Dwyer',
     author_email='richard@richard.do',
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    license='Apache 2.0',
     long_description=readme + '\n\n' + history,
-    long_description_content_type='text/markdown',
     install_requires=install_requires,
     classifiers=[
         'Intended Audience :: Developers',
